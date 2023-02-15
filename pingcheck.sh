751ea6710029e7a1937744602a2e4d93a745d00e
Jon <stricoff92@gmail.com>
2023-02-14 20:51:05 -0500

    wip

---
+++ pingcheck.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOGFILE="/var/log/ispeval-pingcheck.log"
+
+while true; do
+    nowTS=$(date)
+    ping 1.1.1.1 -c 1 -W 1 &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$nowTS no internet" >> $LOGFILE
+        sleep 5
+    else
+        sleep 2
+    fi
+done
